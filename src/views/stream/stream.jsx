2f557fbd3d5fd0f9e3b0c7e260d1d94945a0364a
paulresmer <resmer.paul@gmail.com>
2023-08-11 19:40:32 -0500

    edit topbar, faq

---
+++ src/views/stream/stream.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Container from '../../components/container'
+import Main from '../../layouts/Main';
+
+const Stream = () => {
+  const theme = useTheme();
+  return (
+    <Main>
+      <Box>
+        <Box
+          sx={{
+            backgroundColor: theme.palette.alternate.main,
+            backgroundImage: `linear-gradient(120deg, ${theme.palette.background.paper} 0%, ${theme.palette.alternate.main} 100%)`,
+            marginTop: -13,
+            paddingTop: 13,
+          }}
+        >
+          <Container>
+            {/* <Header /> */}
+          </Container>
+        </Box>
+        <Container maxWidth={800}>
+          {/* <Content /> */}
+        </Container>
+        <Box bgcolor={theme.palette.alternate.main}>
+          <Container>
+            {/* <Footer /> */}
+          </Container>
+        </Box>
+      </Box>
+    </Main>
+  );
+};
+
+export default Stream;
