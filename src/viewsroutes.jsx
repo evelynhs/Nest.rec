a46220e4ce10d0946d6c9716d12e2d3108c85706
paulresmer <resmer.paul@gmail.com>
2023-08-11 20:12:29 -0500

    rename logs

--- src/viewsroutes.jsx
+++ src/viewsroutes.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Stream from './views/stream'
-import Logs from './views/logs'
+import Logs from './views/cam-logs'
 import About from './views/about'
 import Faq from './views/faq'
 
